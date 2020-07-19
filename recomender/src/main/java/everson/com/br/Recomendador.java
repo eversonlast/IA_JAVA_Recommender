b5cd76dc009381b71c9aae4082780163999b5242
eversonlast <eversonlast@yahoo.com.br>
2020-07-19 15:17:29 -0300

    Primeiro Commit

---
+++ recomender/src/main/java/everson/com/br/Recomendador.java
@@ -0,0 +1,11 @@
+package everson.com.br;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Recomendador{
+    
+}
